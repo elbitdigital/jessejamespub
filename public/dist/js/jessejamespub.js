80e9aac3313683b5822a30a15fe0ee5a72798b7a
Cristiano Fromagio <fromagio_cristiano@live.com>
2017-09-07 02:16:02 -0300

    Finished gallery, event page start

--- public/dist/js/jessejamespub.js
+++ public/dist/js/jessejamespub.js
@@ -1,8 +1,3 @@
-/*!
- * Elbit Jesse James Pub Project v0.0.1 (http://elbit.com.br/)
- * Copyright 2013-2017 Elbit Developers
- * Licensed under MIT (https://github.com/elbitdigital/base/blob/master/LICENSE)
-*/
 
 /* Scroll */
 
@@ -193,3 +188,9 @@ var Anchor = (function () {
 	return Anchor;
 
 })();
+
+/*!
+ * Elbit Jesse James Pub Project v0.0.1 (http://elbit.com.br/)
+ * Copyright 2013-2017 Elbit Developers
+ * Licensed under MIT (https://github.com/elbitdigital/base/blob/master/LICENSE)
+*/
