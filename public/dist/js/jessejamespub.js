f4042677b21966b293dddcb22d45984ddf84878e
Cristiano Fromagio <fromagio_cristiano@live.com>
2017-10-11 17:27:36 -0300

    Franqueados page tweak and continuous assembling new index

--- public/dist/js/jessejamespub.js
+++ public/dist/js/jessejamespub.js
@@ -194,3 +194,16 @@ var Anchor = (function () {
  * Copyright 2013-2017 Elbit Developers
  * Licensed under MIT (https://github.com/elbitdigital/base/blob/master/LICENSE)
 */
+
+/* Firebase config */
+
+var config = {
+	apiKey: "AIzaSyAMTCtuJ2DyPiBx5qBHeDg_OU1KW4UzURY",
+	authDomain: "jessejamespub-com-br.firebaseapp.com",
+	databaseURL: "https://jessejamespub-com-br.firebaseio.com",
+	projectId: "jessejamespub-com-br",
+	storageBucket: "jessejamespub-com-br.appspot.com",
+	messagingSenderId: "118227352953"
+};
+
+firebase.initializeApp(config);
